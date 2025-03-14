e9302fe478b2bbd4faca57be1ea4142d9e80fd4f
rachadorr <rachadorr@gmail.com>
2025-03-14 20:49:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const qrcode = require("qrcode-terminal");
 
 const app = express();
 app.use(bodyParser.json());
-const port = process.env.PORT || 3000; // Usa a porta do Railway ou 3000;
+//const port = process.env.PORT || 3000; // Usa a porta do Railway ou 3000;
+const port = 3000; // Usa a porta do Railway ou 3000;
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
 
 
